8807025fcca2e64ff4ec7ae3ea3ea1da88efed9d
Arrosid04 <40455683+Arrosid04@users.noreply.github.com>
2018-06-21 16:31:42 +0800

    Add files via upload
    Start Script for Termux

---
+++ start.py
@@ -0,0 +1,35 @@
+import os, sys
+
+print ("\033[1;31mMasukan Username&Password")
+print ("\033[1;33mKalau Gak tau Pergi Aja Sono")
+username = 'Arrosid04'      
+password = 'Berjalan'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mHalo Selamat Datang di Termux", 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+			print "Back Login\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+		print "Back Login\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
